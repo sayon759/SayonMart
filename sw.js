b3ad5f356db026a63be2d26e7bcc8ada0249166e
sayon759 <sayonkhan49@gmail.com>
2026-08-06 20:55:03 +0530

    Add caching for assets in service worker
    Implement service worker caching for assets.

--- sw.js
+++ sw.js
@@ -1,2 +1,26 @@
-self.addEventListener('install',e=>self.skipWaiting());
-self.addEventListener('fetch',e=>{});
+const CACHE_NAME = "sayonmart-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
